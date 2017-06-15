a9d7a0aa337d4ddabd7ce18dd784634223d1ee0e
Richard Hpa <richard.hpa@hotmail.com>
2017-06-16 11:38:26 +1200

    Added Swiper Files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+  $(document).ready(function () {
+
+    var mySwiper = new Swiper ('.swiper-container',{
+    	initialSlide: 2,
+    	direction: 'vertical',
+    	loop: true,
+    	// autoplay: 2000,
+    	// effect: "fade",
+    	// fade: {
+    	// 	crossFade: true
+    	// }
+    });        
+  });
